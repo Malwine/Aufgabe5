19ac5fe10be8366793a8764d93ccb00cc453c2c3
Malwine Gier <graphicdiaryberlin@gmail.com>
2014-01-11 16:38:24 +0100

    exsample lines tester

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -5,7 +5,24 @@ public class LibraryTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		Book newBook = new Book("titellll", "autoooor", "2009", "Sachbuch", "320918746");
+		
+		/**
+		Book newBook1 = new Book("titellll", "autoooor", "2009", "Sachbuch", "320918746");
+		Book newBook2 = new Book("titellll", "autoooor", "2009", "Sachbuch", "320918746");
+		Book newBook3 = new Book("titellll", "autoooor", "2009", "Sachbuch", "320918746");
+		Book newBook4 = new Book("titellll", "autoooor", "2009", "Sachbuch", "320918746");
+		
+		Library newBib = new Library();
+		
+		newBib.insert(Book newBook);
+		newBib.insert(Book newBook1);
+
+		System.out.println(newBib.print());
+		*/
+		
 		System.out.println(newBook.toString());
 	}
+
 }
