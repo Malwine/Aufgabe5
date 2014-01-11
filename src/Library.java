f145911825fb67cdf1df7d58ed32917830986315
Dangel <dangel@dangel-lappi.(none)>
2014-01-11 11:41:19 -0500

    new method delete Book added

--- src/Library.java
+++ src/Library.java
@@ -3,21 +3,26 @@ public class Library {
 	
 	private int initialCapacity;
 	private int capacityIncrement;
-	String [] Bib;
+	private String [] Bib;
 	
 	public Library()	{
 		initialCapacity = 10;
 		capacityIncrement = 10;
 		Bib = new String [initialCapacity];
-		for (int i = 0; i < Bib.length - 1; i++)	{
-			Bib [i] = "Yeah!";
+	}
+	
+	//public void insert()	{
+		
+	//}
+	
+	public void delete(Book b)	{
+		Bib [b] = "";
+		for (int i = b; i < Bib.length - 2; i++)	{
+			Bib [i] = Bib [i + 1];
 		}
 	}
 	
 	public void print()	{
-		for (int i = 0; i < Bib.length - 1; i++)	{
-			System.out.println(Bib [i]);
-		}
+			System.out.println("x");
 	}
-	
 }
